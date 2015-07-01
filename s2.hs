994d0ec5919d2c5ad00e25f7e038b30bd8fa862a
neo <liangneo@qq.com>
2015-07-01 17:18:00 +0800

    fibs set using iterate

--- s2.hs
+++ s2.hs
@@ -1,8 +1,11 @@
 
 main = print (s2 4000000)
-fibonacci :: Num a => a-> a
+fibonacci :: (Num a,Eq a) => a-> a
 fibonacci x
-	| x == 1 = 1
-	| x == 2  = 2
-	| otherwise = fibonacci (x-1) + fibonacci (x-2)
-s2 n = sum.(filter even).takeWhile(<n).(map fibonacci)$[1..]
+  | x == 1 = 1
+  | x == 2  = 2
+  | otherwise = fibonacci (x-1) + fibonacci (x-2)
+
+fibs = map fst $ iterate (\(m,n)->(n,m+n)) (1,1)
+
+s2 n = sum.(filter even).takeWhile(<n) $ fibs
